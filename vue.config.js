0855569a85f44156aa61a589922fa9a8d8436343
liammccon <liam.t.mccon@gmail.com>
2022-12-04 17:58:30 -0500

    Fix vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/memory-typing-game/'
-  : '/',
-
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
+  : '/'
 }
